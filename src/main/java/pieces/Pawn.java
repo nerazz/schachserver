256a76d025936351fa81cec0336e68f39672b867
letipos <leonardposner@googlemail.com>
2018-11-02 19:03:13 +0100

    test

--- src/main/java/pieces/Pawn.java
+++ src/main/java/pieces/Pawn.java
@@ -5,5 +5,33 @@ package pieces;
  */
 
 public class Pawn extends Piece {
+
+    public String color;
+    public boolean hasMoved;
+    public Coordinate current_coordinate;
+    public Coordinate new_coordinate;
+
+    private Coordinates[] validmoves;
+
+
+    public Pawn(String color){
+        this.color = color;
+        this.hasMoved = false;
+    }
+
+   public boolean valid (Coodinate xy) {
+       if (validmoves.contains(xy))
+           return true;
+       else return false;
+   }
+
+   if(color.equals("white")){
+       validmoves[0] = current_coordinates
+    }
+
+
+
+
+
     //test
 }
--- src/main/java/pieces/Piece.java
+++ src/main/java/pieces/Piece.java
@@ -6,4 +6,11 @@ package pieces;
 
 public class Piece {
 
+        private Board board; // Jeder Spieler hat eogenes Board um Spielstand zu überprüfen
+
+    public int valid (Coodinate xy) {
+        if (validmoves.contains(xy))
+            return 1;
+        else return -1;
+    }
 }
