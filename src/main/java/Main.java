ddd7349ada2165841a4454ecc604eadd3f998778
Niklas Zd <niklas.zd@posteo.de>
2018-11-02 19:01:52 +0100

    added enum PlayerColor, Coord debug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
 import board.Board;
+import players.Player;
+import players.PlayerColor;
 
 public class Main {
     public static void main(String[] args) {
+        Player white = new Player(PlayerColor.WHITE);
+        Player black = new Player(PlayerColor.BLACK);
         Board b = new Board();
         b.init();
         System.out.println(b.toString());
